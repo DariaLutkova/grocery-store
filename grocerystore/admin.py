68811cc67648032297ea7da4457cedd7d2e524e3
GlamDaria <daryalutkova@yandex.ru>
2020-12-31 13:42:47 +0300

    Fill the database

--- grocerystore/admin.py
+++ grocerystore/admin.py
@@ -2,14 +2,54 @@ from django.contrib import admin
 
 from .models import Store, Position, Staff, Supplier, ProductType, Product, Check, LoyaltyCard, Sale, Reviews
 
+class StoreAdmin(admin.ModelAdmin):
+	list_display = ('id', 'address', 'city', 'phone_number')
+	list_display_links = ('id', 'address', 'city', 'phone_number')
+class PositionAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'description', 'isFull')
+	list_display_links = ('id', 'name', 'description', 'isFull')
 
-admin.site.register(Store)
-admin.site.register(Position)
-admin.site.register(Staff)
-admin.site.register(Supplier)
-admin.site.register(ProductType)
-admin.site.register(Product)
-admin.site.register(Check)
-admin.site.register(LoyaltyCard)
-admin.site.register(Sale)
-admin.site.register(Reviews)
+class StaffAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'position', 'store', 'salary')
+	list_display_links =  ('id', 'name', 'position', 'store', 'salary')
+	search_fields = ['name', 'position', 'store']
+
+class SupplierAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'address', 'productType', 'price', 'city')
+	list_display_links = ('id', 'name', 'address', 'productType', 'price', 'city')
+	search_fields = ['name', 'address', 'productType', 'city']
+
+class ProductTypeAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'description')
+	list_display_links = ('id', 'name', 'description')
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'productType', 'supplier', 'store', 'sale', 'price', 'priceSale', 'count', 'date', 'isNew', 'image')
+	list_display_links = ('id', 'name', 'productType', 'supplier', 'store', 'sale', 'price', 'priceSale', 'count', 'date', 'isNew', 'image')
+
+class CheckAdmin(admin.ModelAdmin):
+	list_display = ('id', 'store', 'date', 'total_price', 'loyalty_card')
+	list_display_links = ('id', 'store', 'date', 'total_price', 'loyalty_card')
+
+class LoyaltyCardAdmin(admin.ModelAdmin):
+	list_display = ('id', 'phone_number', 'scores', 'average_check')
+	list_display_links = ('id', 'phone_number', 'scores', 'average_check')
+
+class SaleAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'description')
+	list_display_links = ('id', 'name', 'description')
+class ReviewsAdmin(admin.ModelAdmin):
+	list_display = ('id', 'product', 'name', 'text')
+	list_display_links = ('id', 'product', 'name', 'text')
+
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Check, CheckAdmin)
+admin.site.register(LoyaltyCard, LoyaltyCardAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
--- grocerystore/models.py
+++ grocerystore/models.py
@@ -5,10 +5,16 @@ class Store(models.Model):
 	city = models.CharField(max_length=100)
 	phone_number = models.IntegerField()
 
+	def __str__(self):
+		return self.address
+
 class Position(models.Model):
 	name = models.CharField(max_length=150)
 	description = models.TextField()
 	isFull = models.BooleanField()
+
+	def __str__(self):
+		return self.name
 
 class Staff(models.Model):
 	name = models.CharField(max_length=255)
@@ -19,6 +25,9 @@ class Staff(models.Model):
 	email = models.EmailField()
 	store = models.ForeignKey('Store', related_name='workers', on_delete=models.CASCADE)
 
+	def __str__(self):
+		return self.name
+
 class Supplier(models.Model):
 	name = models.CharField(max_length=150)
 	address = models.CharField(max_length=150)
@@ -26,11 +35,18 @@ class Supplier(models.Model):
 	productType = models.ForeignKey('ProductType', related_name='suppliers', on_delete=models.CASCADE)
 	price = models.FloatField()
 
+	def __str__(self):
+		return self.name
+
 class ProductType(models.Model):
 	name = models.CharField(max_length=150)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
+	name = models.CharField(max_length=150, default = 'default name')
 	productType = models.ForeignKey('ProductType', related_name='products', on_delete=models.CASCADE)
 	supplier = models.ForeignKey('Supplier', related_name='products', on_delete=models.CASCADE)
 	store = models.ForeignKey('Store', related_name='products', on_delete=models.CASCADE)
@@ -42,26 +58,43 @@ class Product(models.Model):
 	isNew = models.BooleanField()
 	image = models.CharField(max_length=150)
 
+	def __str__(self):
+		return self.name
+
+
 class Check(models.Model):
 	store = models.ForeignKey('Store', related_name='checks', on_delete=models.CASCADE)
 	products = models.JSONField()
 	date = models.DateField(auto_now_add=True)
 	total_price = models.FloatField()
 	loyalty_card = models.ForeignKey('LoyaltyCard', related_name='checks', on_delete=models.CASCADE, blank=True)
+	cashier = models.ForeignKey('Staff', related_name='checks', on_delete=models.CASCADE, default = 0)
+
+	def __str__(self):
+		return self.id
 
 class LoyaltyCard(models.Model):
 	phone_number = models.IntegerField()
 	scores = models.IntegerField()
 	average_check = models.FloatField()
 
+	def __str__(self):
+		return self.phone_number
+
 class Sale(models.Model):
 	name = models.CharField(max_length=150)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name
+
 class Reviews(models.Model):
 	name = models.CharField(max_length=150)
 	text = models.TextField()
-	product = models.ForeignKey('Product', related_name='products', on_delete=models.CASCADE,)
+	product = models.ForeignKey('Product', related_name='products', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
 
 
 
